3274edaf7b8e0609fe639d1ab171955b05f72e5b
gottlenz7 <lera.gindulina7@gmail.com>
2026-04-21 22:07:27 +1000

    add big levers and boxes

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -2,19 +2,41 @@ using UnityEngine;
 
 public class Throw : MonoBehaviour
 {
-    public Transform throwItem;
+    public static Throw Instance { get; private set; }
 
-    private float throwForce = 20f, throwUpForce = 5f;
+    public Transform throwItem;
+    public Transform pointA, pointB;
+    public Transform targetPoint;
+    public bool isHit = false;
+
+    private float throwForce = 10f, throwUpForce = 3f;
     private float facingDirection;
+
+    private float speed = 5f;
+
     private Vector2 throwDirection;
     private Rigidbody2D rb;
     private Collider2D itemCollider;
 
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
         throwItem = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
         itemCollider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        SwitchLevers();
     }
 
     public void ThrowItem()
@@ -36,6 +58,21 @@ public class Throw : MonoBehaviour
 
             throwDirection = new Vector2(facingDirection, throwUpForce / throwForce);
             rb.linearVelocity = throwDirection * throwForce;
+        }
+    }
+
+    public void SwitchLevers()
+    {
+        if (isHit)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, targetPoint.position) < 0.5f)
+            {
+                rb.isKinematic = true;
+                rb.linearVelocity = Vector2.zero;
+                targetPoint = (targetPoint == pointA) ? pointB : pointA;
+            }
         }
     }
 }
